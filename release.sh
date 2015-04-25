868c1ecde1d97940f14c1855d7cd4ce9cb24b2b0
Jacques Frechet <zaphod-git@toastball.net>
2015-04-24 20:42:23 -0700

    rebuild with new version number

--- release.sh
+++ release.sh
@@ -6,6 +6,7 @@ test -z "`git status --porcelain 2>&1`"
 ./build.sh
 test -z "`git status --porcelain 2>&1`"
 npm version --git-tag-version=false "$@"
+./build.sh
 version="v`node -e 'console.log(JSON.parse(require("fs").readFileSync("package.json", "utf8")).version)'`"
 git commit -am "$version"
 git tag -am "$version" "$version"
