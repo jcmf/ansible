5e1cdaed4dd7f3a8f72bc4f5932915ed3042350c
Jacques Frechet <zaphod-git@toastball.net>
2015-04-24 20:35:53 -0700

    oops, haven't released the source code yet

--- release.sh
+++ release.sh
@@ -9,5 +9,6 @@ npm version --git-tag-version=false "$@"
 version="v`node -e 'console.log(JSON.parse(require("fs").readFileSync("package.json", "utf8")).version)'`"
 git commit -am "$version"
 git tag -am "$version" "$version"
-git push
+# git push
+# npm publish
 ssh toastball.net 'cat > tmp && test -s tmp && mv tmp www/toastball.net/ansible/index.html' < index.html
