aadd802169958d41df6cee45ff0328ced48750e6
Jacques Frechet <zaphod-git@toastball.net>
2015-04-25 21:26:28 -0700

    lock it down

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd "`dirname $0`"
 set -ex
-node -e 'require("imbroglio").play.compile(require("./game").src)'
+node -e 'require("imbroglio").play.compile(require("./game").src, {handleError: function(e) { throw new Error(e); }})'
 ./node_modules/.bin/gribbl *.jade
